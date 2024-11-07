4d79585775366071c1efdd407fa743d0b45e4f30
Aryan Absalan <aryan.absalan@gmail.com>
2024-11-07 20:08:09 +0100

    saved decision_tree_evaluation_reportas json

--- src/models_module_def/model_evaluation.py
+++ src/models_module_def/model_evaluation.py
@@ -10,6 +10,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from src.entity import ModelEvaluationConfig
 from src.common_utils import save_json
 from custom_logger import logger
+import json
 
 # To fill in with your repo information
 dagshub.init(repo_owner='aryan.absalan', repo_name='RoadAccidentsInFrance', mlflow=True)
@@ -64,11 +65,36 @@ class ModelEvaluation:
                 print(f"Error while logging model: {str(e)}")
                 logger.exception(f"Error while logging model: {str(e)}")
 
+    # def generate_report(self, accuracy, precision, recall, f1):
+    #     report_dir = Path("reports")
+    #     report_dir.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+    #     report_path = report_dir / "decision_tree_evaluation_report.txt"
+    #     with open(report_path, "w", encoding='utf-8') as f:
+    #         f.write(f"Accuracy: {accuracy}\nPrecision: {precision}\nRecall: {recall}\nF1 Score: {f1}\n")
+    #     logger.info(f"Report saved at {report_path}")
     def generate_report(self, accuracy, precision, recall, f1):
+        # Directory for reports
         report_dir = Path("reports")
         report_dir.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+
+        # Path for the JSON report
+        json_report_path = report_dir / "decision_tree_evaluation_report.json"
+        report_data = {
+            "accuracy": accuracy,
+            "precision": precision,
+            "recall": recall,
+            "f1_score": f1
+        }
+        
+        # Writing to JSON file
+        with open(json_report_path, "w", encoding='utf-8') as json_file:
+            json.dump(report_data, json_file, indent=4)
+        logger.info(f"JSON report saved at {json_report_path}")
+        
+        # Path for the text report
         report_path = report_dir / "decision_tree_evaluation_report.txt"
-        with open(report_path, "w", encoding='utf8') as f:
+        with open(report_path, "w", encoding='utf-8') as f:
             f.write(f"Accuracy: {accuracy}\nPrecision: {precision}\nRecall: {recall}\nF1 Score: {f1}\n")
-        logger.info(f"Report saved at {report_path}")
+        logger.info(f"Text report saved at {report_path}")
+
 
